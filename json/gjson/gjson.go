a98b0cbcf2d1eeaa8a5703cefe6edbb4d4405f3a
AlvinFu <AlvinFu@boyaa.com>
2020-01-13 18:05:26 +0800

    update

--- json/gjson/gjson.go
+++ json/gjson/gjson.go
@@ -8,6 +8,6 @@ import (
 const json = `{"name":[{"first":"Janet","last":"Prichard"}, {"abc": 1, "e" : 2}],"age":47}`
 
 func GJson() {
-	rue := gjson.Get(json, "name").Get("last")
+	rue := gjson.Get(json, "name").Get("abc")
 	fmt.Println(rue.Type.String(), rue.String())
 }
