5218ef3dd7965c51585f323ff9603f7a1098ed17
AlvinFu <AlvinFu@boyaa.com>
2020-01-13 18:29:26 +0800

    update

--- json/gjson/gjson.go
+++ json/gjson/gjson.go
@@ -11,3 +11,12 @@ func GJson() {
 	rue := gjson.Get(json, "name").Get("abc")
 	fmt.Println(rue.Type.String(), rue.String())
 }
+func selector(value interface{}) {
+	switch v := value.(type) {
+	case string:
+		fmt.Println(v)
+	case int64:
+	default:
+
+	}
+}
