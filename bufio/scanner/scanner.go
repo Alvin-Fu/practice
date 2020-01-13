88f5b0584fc4d0d425f06940bcd26cb0a944fa9f
AlvinFu <AlvinFu@boyaa.com>
2020-01-13 17:20:26 +0800

    update

--- bufio/scanner/scanner.go
+++ bufio/scanner/scanner.go
@@ -13,10 +13,15 @@ func Scanner() {
 	name := "../readme.md"
 	file, err := os.Open(name)
 	if err != nil {
-		log.Fatalf("failed to open file: %s", name)
+		log.Fatalf("failed to open file: %s, err: %v", name, err)
 		return
 	}
 	defer file.Close()
+	fileInfo, err := file.Stat()
+	if err != nil {
+		log.Fatalf("file stat err: %v", err)
+		return
+	}
 
 	bufio.NewScanner()
 
