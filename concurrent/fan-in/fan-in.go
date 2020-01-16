6a1bc72e81f7aa1a1e318aa43edf98dc65ce8c31
AlvinFu <AlvinFu@boyaa.com>
2020-01-16 17:56:53 +0800

    update

--- concurrent/fan-in/fan-in.go
+++ concurrent/fan-in/fan-in.go
@@ -1,13 +1,36 @@
 package main
 
-import "time"
+import (
+	"time"
+	"fmt"
+)
 
-func main(){}
+func main(){
+	ch := make(chan int)
+	out := make(chan int)
+	go producer(ch, 100 * time.Millisecond)
+	go producer(ch, 200 *time.Millisecond)
+	go reader(out)
+	for i := range ch{
+		out <- i
+	}
 
-func producer(ch chan int, d time.Duration){}
+}
+
+func producer(ch chan int, d time.Duration){
+	var i int
+	for {
+		ch <- i
+		i ++
+		time.Sleep(d)
+		if i == 10{
+			break
+		}
+	}
+}
 
 func reader(out chan int){
-	for {
-		
+	for x := range out{
+		fmt.Println(x)
 	}
 }
