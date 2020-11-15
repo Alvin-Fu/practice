19b70d330a958d9ba3fba8b78c435a23d338b78d
Alvin-Fu <fuli314@outlook.com>
2020-11-15 16:21:10 +0800

    fix: 对json和fmt中的%v的练习

---
+++ basic/tfmt/fmt.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+type People struct {
+	Name string
+}
+
+func (p People) String() string {
+	return fmt.Sprintf("print: %#v", p)
+}
+
+func main() {
+	p := People{}
+	p.String()
+}
