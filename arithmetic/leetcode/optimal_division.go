71bd9ff788c8672cc909e4ff4115ba1c463ca281
theodorefu <theodorefu@tencent.com>
2022-02-28 10:06:20 +0800

    fix: 最优除法

--- arithmetic/leetcode/optimal_division.go
+++ arithmetic/leetcode/optimal_division.go
@@ -1,1 +1,22 @@
 package main
+
+import "fmt"
+
+func main() {
+
+}
+
+func optimalDivision(nums []int) string {
+	if len(nums) == 1 {
+		return fmt.Sprintf("%d", nums[0])
+	}
+	if len(nums) == 2 {
+		return fmt.Sprintf("%d/%d", nums[0], nums[1])
+	}
+	rue := fmt.Sprintf("%d/(%d", nums[0], nums[1])
+	for i := 2; i < len(nums); i++ {
+		rue += fmt.Sprintf("/%d", nums[i])
+	}
+	rue += ")"
+	return rue
+}
